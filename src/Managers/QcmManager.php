4e864ea9ab4ef06f6d104f233d1b014c1c630e24
Kazk12 <kazuko43KO@gmail.com>
2025-01-14 09:09:16 +0100

    QcmManager

--- src/Managers/QcmManager.php
+++ src/Managers/QcmManager.php
@@ -71,3 +71,42 @@ final class QcmManager
     //     }
 
 }
+?>
+
+
+<?php
+
+class QcmManager {
+    
+    public function generateDisplay(Qcm $qcm) {
+        
+        ob_start();
+        
+        ?>
+        <section>
+            <h1>
+                Test
+            </h1>
+            <h2>
+                <?= $qcm->getName() ?>
+            </h2>
+            <?php foreach ($qcm->getQuestions() as $question) { ?>
+                <h3>
+                    <?= $question->getNameQuestion() ?>
+                </h3>
+                <ul>
+                    <?php foreach ($question->getAnswers() as $answer) { ?>
+                        <li>
+                            <?= $answer->getNameAnswer() ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            <?php } ?>
+        </section>
+        <?php
+        
+        return ob_get_clean();
+    }
+}
+
+?>
